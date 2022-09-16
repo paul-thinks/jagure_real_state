3a04dc9f60d6c9ba4d0bf3e3035ee0233d77a4c6
sajal paul <111637993+paul-thinks@users.noreply.github.com>
2022-09-16 09:07:21 +0530

    Add files via upload

---
+++ real-estate website/js/file.js
@@ -0,0 +1,15 @@
+console.log('realstate website using html,css,and js');
+
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+}
